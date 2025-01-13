16ef9193740cfdfb9c723300cfa34fce95c8b1c9
Thomas Chao <Tchao@lcs.on.ca>
2025-01-13 09:23:50 -0500

    Initial Commit

---
+++ Sum of numbers/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Sum of numbers
+//
+//  Created by Yuk Yeung Chao on 2025-01-13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Sum of numbers/Sum_of_numbersApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sum_of_numbersApp.swift
+//  Sum of numbers
+//
+//  Created by Yuk Yeung Chao on 2025-01-13.
+//
+
+import SwiftUI
+
+@main
+struct Sum_of_numbersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
